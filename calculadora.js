371ef37b6dc51b4e0ea2e88a1ea6d9179ff8f98c
Geronimo Olanda <51466744+GeronimoOlanda@users.noreply.github.com>
2020-06-23 20:39:45 -0300

    Add files via upload

---
+++ calculadora.js
@@ -0,0 +1,33 @@
+let valor1 = 20;
+let valor2 = 30;
+
+
+
+let soma = function(valor1,valor2){
+     somar = valor1+valor2;
+     return somar;
+}
+
+let menos = function(valor1,valor2){
+    m = valor1-valor2;
+
+    return m;
+}
+
+let mult = function(valor1,valor2){
+    m = valor1*valor2;
+
+    return m;
+}
+
+let div = function(valor1,valor2){
+    di = valor1/valor2;
+
+    return di;
+}
+
+
+console.log(soma(20,39))
+console.log(menos(20,39))
+console.log(mult(20,39))
+console.log(div(20,39))
